df6223d99ae237a4eab2bd462f83116e2a40c101
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-07-01 20:29:57 +0300

    Написал функцию createRequest

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,10 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
-  /**
-   * Получает информацию о счёте
-   * */
-  static get(id = '', callback){
+    /**
+     * Получает информацию о счёте
+     * */
+    static get(id = '', callback) {
 
-  }
+    }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,35 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest();
+    let url = options.url;
+    let formData = null;
+    xhr.responseType = 'json';
 
+    if (options.method === 'GET' && options.data) {
+        // Формируем url для GET-запроса (перебираем все свойства объекта options.data и записываем в переменную url)
+        url += '?';
+        for (let name in options.data) {
+            url += `${name}=${options.data[name]}&`;
+        }
+        url = url.substr(0, url.length - 1); // Удаляем последний символ строки (&)
+    } else if (options.data) {
+        // Создаём объект formData и записываем в него все свойства объекта options.data
+        formData = new FormData();
+        for (let name in options.data) {
+            formData.append(name, options.data[name]);
+        }
+    }
+
+    // Открываем соединение в соотствествии с методом и отправляем на сервер сформированные данные.
+    // В случае возникновения ошибки, передаём её в функцию callback, находящуюся в объекте options:
+    try {
+        xhr.open(options.method, url);
+        xhr.send(formData);
+    } catch (err) {
+        options.callback(err);
+    }
+
+    // В случае успешного выполнения кода вызываем функцию callback, находящуюся в объекте options:
+    xhr.onload = () => options.callback(null, xhr.response);
 };
